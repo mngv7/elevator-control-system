96360d8c037c80029657f817c029e93d4e5f1b04
mngv7 <zackariya.taylor@gmail.com>
2024-09-25 11:19:32 +1000

    Implemented command line reading as well as input checkers.

--- call.c
+++ call.c
@@ -12,8 +12,7 @@
 #include <unistd.h>
 #include <errno.h>
 
-void send_looped(int fd, const void *buf, size_t sz)
-{
+void send_looped(int fd, const void *buf, size_t sz) {
     const char *ptr = buf;
     size_t remain = sz;
 
@@ -28,29 +27,44 @@ void send_looped(int fd, const void *buf, size_t sz)
     }
 }
 
-void send_message(int fd, const char *buf)
-{
+void send_controller_message(int fd, const char *buf) {
     uint32_t len = htonl(strlen(buf));
     send_looped(fd, &len, sizeof(len));
     send_looped(fd, buf, strlen(buf));
 }
 
-int main (int argc, char **argv) 
-{
+int main (int argc, char **argv) {
 if (argc != 3 ) {
     printf("Usage: {source floor} {destination floor}\n");
     exit(1);
 }
 
-char source_floor[4];
+char current_floor[4];
+char destination_floor[4];
 
-
-for (int i = 0; i < sizeof(argv[1])/4; i++) 
-{
-    source_floor[i] = argv[1][i];
+for (int i = 0; i < strlen(argv[1]); i++) {
+    if (strlen(argv[1]) > 3) {
+    printf("Invalid floor(s) specified.\n");
+        exit(EXIT_FAILURE);
+    }
+    current_floor[i] = argv[1][i];
 }
 
-printf("%s", source_floor);
+for (int i = 0; i < strlen(argv[2]); i++) {
+    if (strlen(argv[2]) > 3) {
+    printf("Invalid floor(s) specified.\n");
+        exit(EXIT_FAILURE);
+    }
+    destination_floor[i] = argv[2][i];
+}
 
-return 0;
+if (!strcmp(current_floor, destination_floor)) {
+    printf("You are already on that floor!\n");
+    exit(EXIT_FAILURE);
+}
+
+printf("Current floor: %s\n", current_floor);
+printf("Destination floor: %s\n", destination_floor);
+
+return EXIT_SUCCESS;
 }
