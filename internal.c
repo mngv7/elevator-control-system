5b2dbb0d598f33db91caf9c3aa6265a367da6404
mngv7 <zackariya.taylor@gmail.com>
2024-09-25 12:41:25 +1000

    Created main method for internal, added required header files.

---
+++ internal.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <errno.h>
+
+int main(int argc, char **argv)
+{
+
+}
