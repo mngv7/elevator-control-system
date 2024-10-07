633e3293344413c306b63ddd0f3e33be2812ddc9
mngv7 <zackariya.taylor@gmail.com>
2024-10-07 11:25:16 +1000

    Created two threads for waiting for cond signal and for updating shared memory, created a function for printing, without using stdio.

--- safety.c
+++ safety.c
@@ -14,23 +14,30 @@
 #include <fcntl.h>
 #include <signal.h>
 
+void *wait_for_signal_thread(void *ptr);
+void *update_shared_mem_thread(void *ptr);
+void custom_print(const char *str);
+
+typedef struct
+{
+    pthread_mutex_t mutex;
+    pthread_cond_t cond;
+    char current_floor[4];
+    char destination_floor[4];
+    char status[8];
+    uint8_t open_button;
+    uint8_t close_button;
+    uint8_t door_obstruction;
+    uint8_t overload;
+    uint8_t emergency_stop;
+    uint8_t individual_service_mode;
+    uint8_t emergency_mode;
+} car_shared_mem;
+
 int main(int argc, char **argv)
 {
-    typedef struct
-    {
-        pthread_mutex_t mutex;
-        pthread_cond_t cond;
-        char current_floor[4];
-        char destination_floor[4];
-        char status[8];
-        uint8_t open_button;
-        uint8_t close_button;
-        uint8_t door_obstruction;
-        uint8_t overload;
-        uint8_t emergency_stop;
-        uint8_t individual_service_mode;
-        uint8_t emergency_mode;
-    } car_shared_mem;
+    pthread_t consumer;
+    pthread_t producer;
 
     car_shared_mem *ptr;
 
@@ -55,12 +62,6 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    if (close(shm_fd) == -1)
-    {
-        printf("Error closing shared memory file descriptor: %s\n", strerror(errno));
-        return 1;
-    }
-
     ptr = mmap(0, sizeof(car_shared_mem), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
 
     if (ptr == MAP_FAILED)
@@ -69,5 +70,82 @@ int main(int argc, char **argv)
         exit(1);
     }
 
+    pthread_create(&consumer, NULL, wait_for_signal_thread, ptr);
+    pthread_create(&producer, NULL, update_shared_mem_thread, ptr);
+
+    if (pthread_join(consumer, NULL) != 0)
+    {
+        printf("pthread join fail.\n");
+        return 1;
+    }
+
+    if (pthread_join(producer, NULL) != 0)
+    {
+        printf("pthread join fail.\n");
+        return 1;
+    }
+
     return 0;
+}
+
+void *wait_for_signal_thread(void *ptr)
+{
+    car_shared_mem *shared_mem = (car_shared_mem *)ptr;
+
+    while (1)
+    {
+        pthread_mutex_lock(&shared_mem->mutex);
+
+        // Wait for the condition signal
+        pthread_cond_wait(&shared_mem->cond, &shared_mem->mutex);
+
+        pthread_mutex_unlock(&shared_mem->mutex);
+
+        usleep(100000); // Sleep for 100ms
+    }
+
+    return NULL;
+}
+
+void *update_shared_mem_thread(void *ptr)
+{
+    car_shared_mem *shared_mem = (car_shared_mem *)ptr;
+
+    while (1)
+    {
+        pthread_mutex_lock(&shared_mem->mutex);
+
+        if (shared_mem->door_obstruction == 1 && strcmp(shared_mem->status, "Closing") == 0)
+        {
+            strcpy(shared_mem->status, "Opening");
+        }
+
+        if ((shared_mem->emergency_stop == 1) && (shared_mem->emergency_mode == 0))
+        {
+            custom_print("The emergency stop button has been pressed!\n");
+        }
+
+        pthread_mutex_unlock(&shared_mem->mutex);
+
+        // Signal the condition variable to wake up the other thread
+        pthread_cond_signal(&shared_mem->cond);
+
+        usleep(100000); // Sleep for 100ms
+    }
+
+    return NULL;
+}
+
+void custom_print(const char *str)
+{
+    if (str != NULL)
+    {
+        size_t length = strlen(str); // Get the length directly
+
+        ssize_t bytes_written = write(1, str, length);
+        if (bytes_written < 0)
+        {
+            perror("write error");
+        }
+    }
 }
