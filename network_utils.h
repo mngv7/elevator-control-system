1d5e3f13b65f4ba46e772a201ee22335f1deaa66
mngv7 <zackariya.taylor@gmail.com>
2024-10-27 09:53:51 +1000

    Fixed an error in controller where it was popping elements of the call list without dispatching trhem

--- network_utils.h
+++ network_utils.h
@@ -8,6 +8,7 @@ void recv_looped(int fd, void *buf, size_t sz);
 void send_looped(int fd, const void *buf, size_t sz);
 void send_message(int fd, const char *buf);
 char *receive_msg(int fd);
-int establish_connection();
+int establish_connection_client();
+int establish_connection_server();
 
 #endif // NETWORK_UTILS_H
